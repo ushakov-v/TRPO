31e45a0d38e2e671c1b7133df5c7986733c8794f
Vitaliy Ushakov <vitaliy-ushak0v@ya.ru>
2023-03-14 23:33:52 +0300

    Method foobar

---
+++ function2.rb
@@ -0,0 +1,7 @@
+def foobar(first_number, second_number)
+if (first_number.to_i == 20 || second_number.to_i == 20)
+puts second_number.to_i
+else
+puts first_number.to_i + second_number.to_i
+end
+end
